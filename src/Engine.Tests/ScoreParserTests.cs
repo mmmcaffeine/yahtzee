8cc16bb4fb2736253c46fb1e7928652514017772
Mark Roberts <mroberts@geodesys.com>
2021-10-11 12:34:33 +0100

    ScoreParser can now parse any number of dice rolls
    We are no longer restricted to supplying _exactly_ five dice rolls. We use a named group and capture all occurrences of it, but use lookbehind and lookahead to ensure we're still looking for things inside the parentheses, and being comma separated.

--- src/Engine.Tests/ScoreParserTests.cs
+++ src/Engine.Tests/ScoreParserTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using FluentAssertions;
 using Xunit;
 
@@ -7,18 +8,26 @@ namespace Dgt.Yahtzee.Engine
     public class ScoreParserTests
     {
         // TODO Add theories for when the ScoreParser is less brittle
-        [Fact]
-        public void GetScoreValues_Should_ExtractDiceRollsFromScore()
+        [Theory]
+        [MemberData(nameof(GetGetScoreValuesData))]
+        public void GetScoreValues_Should_ExtractDiceRollsFromScore(string score, IEnumerable<int> expected)
         {
             // Arrange
-            const string score = "(1, 1, 2, 4, 4) fours";
             var sut = new ScoreParser();
             
             // Act
             var scoreValues = sut.GetScoreValues(score).ToList();
             
             // Assert
-            scoreValues.Should().HaveCount(5).And.Contain(new[] { 1, 1, 2, 4, 4 });
+            scoreValues.Should().BeEquivalentTo(expected);
+        }
+
+        private static IEnumerable<object[]> GetGetScoreValuesData()
+        {
+            yield return new object[] { "(6) ones", new[] { 6 } };
+            yield return new object[] { "(3, 5) threes", new[] { 3, 5 } };
+            yield return new object[] { "(1, 1, 2, 4, 4) fours", new[] { 1, 1, 2, 4, 4 } };
+            yield return new object[] { "(6, 5, 3, 3, 5, 1) chance", new[] { 6, 5, 3, 3, 5, 1} };
         }
 
         // TODO Add theories for when the ScoreParser is less brittle
--- src/Engine/ScoreParser.cs
+++ src/Engine/ScoreParser.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
@@ -7,30 +6,25 @@ namespace Dgt.Yahtzee.Engine
 {
     public class ScoreParser : IScoreParser
     {
-        private const string ScoreValuesPattern = @"\((?<scoreValues>\d, \d, \d, \d, \d)\)";
-        private const StringSplitOptions ScoreValuesStringSplitOptions = StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries;
+        private const string ScoreValuesPattern = @"(?<=(\(|, ))(?<scoreValue>\d(?=(, |\))))";
 
         private readonly Regex _scoreValuesRegEx = new(ScoreValuesPattern, RegexOptions.Compiled);
         
         // TODO Validate for null and empty strings
-        // TODO Replace with Regex parsing
         // TODO Throw FormatException if we cannot parse
         // TODO Make more reliable by eliminating the assumptions below
         
         // Assume the input string will be of the form "(1, 1, 2, 4, 4) fours" i.e.
         // * Will always start with an open paren, and no whitespace
-        // * Will always contain five dice rolls
         // * Will always be six-sided die (i.e. the value will be between 1 and 6 and therefore a single digit)
         // * Dice rolls will always be separated by a comma followed by a space
         // * Dice rolls will always end with a close paren
         public IEnumerable<int> GetScoreValues(string score)
         {
-            var match = _scoreValuesRegEx.Match(score);
-            var scoreValues = match.Groups["scoreValues"].Value;
+            var matches = _scoreValuesRegEx.Matches(score);
+            var scoreValues = matches.Select(m => m.Groups["scoreValue"].Value);
             
-            return scoreValues
-                .Split(",", ScoreValuesStringSplitOptions)
-                .Select(int.Parse);
+            return scoreValues.Select(int.Parse);
         }
 
         // TODO Validate for null and empty strings
