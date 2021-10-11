0c7987684d891b0ae7d2901dfd1489cc61129319
Mark Roberts <mroberts@geodesys.com>
2021-10-11 15:29:30 +0100

    Made test data for GetScoreValues strongly typed
    By introducing a class to represent a singe test case we can then construct instances of that type and its constructor ensures we have the correct parameters in the correct order, unlike an `object[]`. We also get the advantage of the parameters being named so we can get a better idea of what we're actually passing!
    
    Because the test case implements an implicit conversion to `object[]` we can still return it in the method that builds all of the test data 😃

--- src/Engine.Tests/ScoreParserTests.cs
+++ src/Engine.Tests/ScoreParserTests.cs
@@ -21,22 +21,39 @@ namespace Dgt.Yahtzee.Engine
             // Assert
             scoreValues.Should().BeEquivalentTo(expected);
         }
-
+        
         private static IEnumerable<object[]> GetGetScoreValuesData()
         {
             // Parse any number of dice rolls
-            yield return new object[] { "(6) ones", new[] { 6 } };
-            yield return new object[] { "(3, 5) threes", new[] { 3, 5 } };
-            yield return new object[] { "(1, 1, 2, 4, 4) fours", new[] { 1, 1, 2, 4, 4 } };
-            yield return new object[] { "(6, 5, 3, 3, 5, 1) chance", new[] { 6, 5, 3, 3, 5, 1} };
+            yield return new GetScoreValuesTestCase("(6) ones", new[] { 6 });
+            yield return new GetScoreValuesTestCase("(3, 5) threes", new[] { 3, 5 });
+            yield return new GetScoreValuesTestCase("(1, 1, 2, 4, 4) fours", new[] { 1, 1, 2, 4, 4 });
+            yield return new GetScoreValuesTestCase("(6, 5, 3, 3, 5, 1) chance", new[] { 6, 5, 3, 3, 5, 1});
             
             // Parse things other than six-side dice e.g. we can work with a D20
-            yield return new object[] { "(10, 12, 7) tens", new[] { 10, 12, 7 } };
-            yield return new object[] { "(4, 15, 9) yahtzee", new[] { 4, 15, 9 } };
+            yield return new GetScoreValuesTestCase("(10, 12, 7) tens", new[] { 10, 12, 7 });
+            yield return new GetScoreValuesTestCase("(4, 15, 9) yahtzee", new[] { 4, 15, 9 });
             
             // Parse things even when we have superfluous whitespace or commas
-            yield return new object[] { "    (   10  ,   17, 7   )    pair", new[] { 10, 17, 7 } };
-            yield return new object[] { "(5, , , 4)", new[] { 5, 4 } };
+            yield return new GetScoreValuesTestCase("    (   10  ,   17, 7   )    pair", new[] { 10, 17, 7 });
+            yield return new GetScoreValuesTestCase("(5, , , 4) ones", new[] { 5, 4 });
+        }
+
+        private class GetScoreValuesTestCase
+        {
+            private string Score { get; }
+            private IEnumerable<int> ExpectedScoreValues { get; }
+
+            public GetScoreValuesTestCase(string score, IEnumerable<int> expectedScoreValues)
+            {
+                Score = score;
+                ExpectedScoreValues = expectedScoreValues;
+            }
+
+            public static implicit operator object[](GetScoreValuesTestCase data)
+            {
+                return new object[] { data.Score, data.ExpectedScoreValues };
+            }
         }
 
         // TODO Add theories for when the ScoreParser is less brittle
