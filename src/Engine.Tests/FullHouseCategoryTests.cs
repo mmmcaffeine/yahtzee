3baff2a2adf11a2f354dbcc2e1c5847259f40470
Mark Roberts <mroberts@geodesys.com>
2021-10-12 07:29:32 +0100

    Implemented FullHouseCategory
    As per the requirements this expects _exactly_ two of a kind, and three of a kind. Some variants of the real game allow a Yahtzee to be scored as a Full House. The official rules, and our requirements specification do not.

---
+++ src/Engine.Tests/FullHouseCategoryTests.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace Dgt.Yahtzee.Engine
+{
+    public class FullHouseCategoryTests
+    {
+        [Fact]
+        public void Name_Should_BeFullHouse()
+        {
+            new FullHouseCategory().Name.Should().BeEquivalentTo("full house");
+        }
+
+        [Theory]
+        [MemberData(nameof(GetGetCategoryScoreTestCases))]
+        public void GetCategoryScore_Should_ReturnCategoryScore(IEnumerable<int> scoreValues, int expectedCategoryScore)
+        {
+            // Arrange
+            var sut = new FullHouseCategory();
+
+            // Act
+            var categoryScore = sut.GetCategoryScore(scoreValues);
+
+            // Assert
+            categoryScore.Should().Be(expectedCategoryScore);
+        }
+
+        private static IEnumerable<object[]> GetGetCategoryScoreTestCases()
+        {
+            yield return new GetCategoryScoreTestCase(new[] { 1, 1, 3, 3 }, 0);         // Less than five dice
+            yield return new GetCategoryScoreTestCase(new[] { 1, 1, 1, 3, 3, 3 }, 0);   // More than five dice
+            yield return new GetCategoryScoreTestCase(new[] { 1, 2, 3, 4, 5 }, 0);      // Not a combination of two and three of a kind
+            yield return new GetCategoryScoreTestCase(new[] { 4, 4, 1, 4, 4 }, 0);      // Not a combination of two and three of a kind
+            yield return new GetCategoryScoreTestCase(new[] { 5, 5, 5, 5, 5 }, 0);      // Five of a kind, _not_ two and three of a kind
+            yield return new GetCategoryScoreTestCase(new[] { 1, 1, 3, 3, 3 }, 11);     // Two of a kind, and three of a kind
+            yield return new GetCategoryScoreTestCase(new[] { 5, 5, 5, 3, 3 }, 21);     // Three of a kind, and two of a kind
+            yield return new GetCategoryScoreTestCase(new[] { 2, 4, 2, 2, 4 }, 14);     // Two of a kind, and three of a kind, but not in sequence
+        }
+
+        private class GetCategoryScoreTestCase
+        {
+            private IEnumerable<int> ScoreValues { get; }
+            private int ExpectedCategoryScore { get; }
+            
+            public GetCategoryScoreTestCase(IEnumerable<int> scoreValues, int expectedCategoryScore)
+            {
+                ScoreValues = scoreValues;
+                ExpectedCategoryScore = expectedCategoryScore;
+            }
+
+            public static implicit operator object[](GetCategoryScoreTestCase testCase) =>
+                new object[] { testCase.ScoreValues, testCase.ExpectedCategoryScore };
+        }
+    }
+}
---
+++ src/Engine/FullHouseCategory.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Dgt.Yahtzee.Engine
+{
+    public class FullHouseCategory : ICategory
+    {
+        public string Name => "full house";
+        
+        // TODO Parameter validation - no nulls
+        public int GetCategoryScore(IEnumerable<int> scoreValues)
+        {
+            var groups = scoreValues.GroupBy(x => x).ToList();
+            var correctGrouping = groups.Count == 2
+                                  && groups.Sum(group => group.Count()) == 5
+                                  && groups.SingleOrDefault(group => group.Count() == 2) is not null;
+
+            return correctGrouping
+                ? groups.Sum(group => group.Key * group.Count())
+                : 0;
+        }
+    }
+}
