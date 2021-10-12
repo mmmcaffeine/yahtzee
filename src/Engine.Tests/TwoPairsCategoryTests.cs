18b36864c41cfcc713c014453e6eb2e64b4236dd
Mark Roberts <mroberts@geodesys.com>
2021-10-12 13:03:41 +0100

    Implemented TwoPairsCategory
    The specification was a little unclear about how to handle certain cases such as when we have a round that would technically match a Yahtzee or Full House. How I have chosen to handle these has been explicitly defined in the unit tests. I have also made the assumption this specific category is to be used for "classic" Yahtzee i.e. with rounds of five dice. As such cases such as six dice with two or three pairs do not score. Again, this is defined in the unit tests.

---
+++ src/Engine.Tests/TwoPairsCategoryTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace Dgt.Yahtzee.Engine
+{
+    public class TwoPairsCategoryTests
+    {
+        [Fact]
+        public void Name_Should_BeTwoPairs()
+        {
+            new FullHouseCategory().Name.Should().BeEquivalentTo("full house");
+        }
+        
+        [Theory]
+        [MemberData(nameof(GetGetCategoryScoreTestCases))]
+        public void GetCategoryScore_Should_ReturnSumOfPairsOfDice(IEnumerable<int> scoreValues, int expectedCategoryScore)
+        {
+            // Arrange
+            var sut = new TwoPairsCategory();
+
+            // Act
+            var categoryScore = sut.GetCategoryScore(scoreValues);
+
+            // Assert
+            categoryScore.Should().Be(expectedCategoryScore);
+        }
+        
+        private static IEnumerable<object[]> GetGetCategoryScoreTestCases()
+        {
+            yield return new GetCategoryScoreTestCase(new[] { 1, 1, 3, 3 }, 0);         // Two pairs, but less than five dice
+            yield return new GetCategoryScoreTestCase(new[] { 1, 1, 3, 3, 5, 6 }, 0);   // Twp pairs, but more than five dice
+            yield return new GetCategoryScoreTestCase(new[] { 1, 2, 3, 4, 5 }, 0);      // No pairs
+            yield return new GetCategoryScoreTestCase(new[] { 1, 1, 3, 4, 5 }, 0);      // Only one pair
+            yield return new GetCategoryScoreTestCase(new[] { 2, 2, 4, 4, 5 }, 12);     // Two pairs of different face values
+            yield return new GetCategoryScoreTestCase(new[] { 4, 4, 1, 4, 4 }, 16);     // Two pairs of the same face values
+            yield return new GetCategoryScoreTestCase(new[] { 4, 1, 3, 4, 3 }, 14);     // Two pairs, but not in sequence 
+            yield return new GetCategoryScoreTestCase(new[] { 1, 1, 3, 3, 3 }, 8);      // Full house, but still has two pairs!
+            yield return new GetCategoryScoreTestCase(new[] { 5, 5, 5, 5, 5 }, 20);     // Yahtzee, but still has two pairs!
+        }
+        
+        private class GetCategoryScoreTestCase
+        {
+            private IEnumerable<int> ScoreValues { get; }
+            private int ExpectedCategoryScore { get; }
+            
+            public GetCategoryScoreTestCase(IEnumerable<int> scoreValues, int expectedCategoryScore)
+            {
+                ScoreValues = scoreValues;
+                ExpectedCategoryScore = expectedCategoryScore;
+            }
+
+            public static implicit operator object[](GetCategoryScoreTestCase testCase) =>
+                new object[] { testCase.ScoreValues, testCase.ExpectedCategoryScore };
+        }
+    }
+}
---
+++ src/Engine/TwoPairsCategory.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Dgt.Yahtzee.Engine
+{
+    public class TwoPairsCategory : ICategory
+    {
+        public string Name => "two pairs";
+        
+        // TODO Parameter validation - no nulls
+        public int GetCategoryScore(IEnumerable<int> scoreValues)
+        {
+            var groupings = scoreValues.GroupBy(x => x).ToList();
+            
+            if (groupings.Sum(x => x.Count()) != 5)
+            {
+                return 0;
+            }
+            
+            var pairs = groupings
+                .Select(grouping => (value: grouping.Key, count: grouping.Count() % 2 == 0 ? grouping.Count() : grouping.Count() - 1))
+                .ToList();
+            
+            return pairs.Sum(pair => pair.count) == 4
+                ? pairs.Sum(pair => pair.value * pair.count)
+                : 0;
+        }
+    }
+}
