3aaade4aa7953c83afd14dbc879b9b0bcd44fab0
Mark Roberts <mroberts@geodesys.com>
2021-12-18 10:54:53 +0000

    Added basic test that GameScorer cannot re-use the same ICategory

--- src/Engine.Tests/GameScorerTests.cs
+++ src/Engine.Tests/GameScorerTests.cs
@@ -65,5 +65,34 @@ namespace Dgt.Yahtzee.Engine
                 .Should().Throw<InvalidOperationException>()
                 .And.Message.Should().StartWith("The score for the round did not match any of the available categories.");
         }
+
+        // TODO Enhance the exception check
+        // * It should indicate the score in some way
+        // * It should indicate the categories in some way, specifically the category we tried to play again
+        // * It should indicate the category selector in some way
+        // It might make sense to put these in Data if we don't want to build a custom exception type
+        // It might make sense to suffix these onto Message if we don't want to build a custom exception type
+        [Fact]
+        public void GetRoundScore_Should_NotAllowSameCategoryToBeUsedMoreThanOnce()
+        {
+            // Arrange
+            var fakeScoreParser = A.Fake<IScoreParser>();
+            A.CallTo(() => fakeScoreParser.GetCategoryName(A<string>._)).Returns("full house");
+            
+            var fakeCategory = A.Fake<ICategory>();
+            A.CallTo(() => fakeCategory.Name).Returns("full house");
+
+            var fakeCategorySelector = A.Fake<ICategorySelector>();
+            A.CallTo(() => fakeCategorySelector.SelectCategory(An<IEnumerable<int>>._, "full house", An<IEnumerable<ICategory>>._))
+                .Returns(fakeCategory);
+
+            var sut = new GameScorer(fakeScoreParser, new[] { fakeCategory }, fakeCategorySelector);
+            _ = sut.GetRoundScore("(3, 3, 5, 5, 5) full house");
+            
+            // Act, Assert
+            sut.Invoking(x => x.GetRoundScore("(1, 1, 6, 6, 6) full house"))
+                .Should().Throw<InvalidOperationException>()
+                .And.Message.Should().StartWith("The score for the round tried to use a category that has already been used.");
+        }
     }
 }
--- src/Engine/GameScorer.cs
+++ src/Engine/GameScorer.cs
@@ -9,6 +9,7 @@ namespace Dgt.Yahtzee.Engine
         private readonly IScoreParser _scoreParser;
         private readonly IEnumerable<ICategory> _categories;
         private readonly ICategorySelector _categorySelector;
+        private readonly List<ICategory> _playedCategories = new();
 
         // TODO Parameter validation - no nulls, but an empty enumerable is allowed
         public GameScorer(IScoreParser scoreParser, IEnumerable<ICategory> categories, ICategorySelector categorySelector)
@@ -24,14 +25,22 @@ namespace Dgt.Yahtzee.Engine
             var categoryName = _scoreParser.GetCategoryName(score);
             var category = _categorySelector.SelectCategory(scoreValues, categoryName, _categories);
 
-            return category is not null
-                ? category.GetCategoryScore(scoreValues)
-                : throw CreateExceptionForNoMatchingCategory();
+            if (category is null) throw CreateExceptionForNoMatchingCategory();
+            if (_playedCategories.Contains(category)) throw CreateExceptionForCategoryAlreadyPlayed();
+
+            var categoryScore = category.GetCategoryScore(scoreValues);
+            _playedCategories.Add(category);
+            return categoryScore;
         }
 
         private static Exception CreateExceptionForNoMatchingCategory()
         {
             return new InvalidOperationException("The score for the round did not match any of the available categories.");
+        }
+
+        private static Exception CreateExceptionForCategoryAlreadyPlayed()
+        {
+            return new InvalidOperationException("The score for the round tried to use a category that has already been used.");
         }
     }
 }
